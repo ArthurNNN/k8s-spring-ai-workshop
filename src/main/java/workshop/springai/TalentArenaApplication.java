407620274348f55f35272c17ade258cbbeddd6b9
bantunes82 <bantunes82@gmail.com>
2025-02-06 17:50:45 +0100

    Removing k8s from the project

--- src/main/java/workshop/springai/TalentArenaApplication.java
+++ src/main/java/workshop/springai/TalentArenaApplication.java
@@ -1,4 +1,4 @@
-package workshop.k8s.springai;
+package workshop.springai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/workshop/springai/TalentArenaApplicationTests.java
+++ src/test/java/workshop/springai/TalentArenaApplicationTests.java
@@ -1,4 +1,4 @@
-package workshop.k8s.springai;
+package workshop.springai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
