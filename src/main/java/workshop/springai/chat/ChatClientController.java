462c706aac0c1df30359f15413d6fd6935932650
bantunes82 <bantunes82@gmail.com>
2025-02-07 21:57:25 +0100

    Adding a chat client with Memory to interact with a chat model

--- src/main/java/workshop/springai/chat/ChatClientController.java
+++ src/main/java/workshop/springai/chat/ChatClientController.java
@@ -1,27 +1,45 @@
 package workshop.springai.chat;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class ChatClientController {
 
     private final ChatClient chatClient;
+    private final ChatMemory chatMemory;
 
     @Autowired
-    public ChatClientController(ChatClient chatClient) {
+    public ChatClientController(ChatClient chatClient, ChatMemory chatMemory) {
         this.chatClient = chatClient;
+        this.chatMemory = chatMemory;
     }
 
     @GetMapping(value = "/chat", produces = "text/plain", consumes = "application/json")
-    String chat(@RequestBody(required = true) String message) {
+    String chat(@RequestBody String message) {
+        log.info("Chatting with message: {}", message);
 
         return chatClient.prompt()
                 .user(message)
+                .call()
+                .content();
+    }
+
+    @GetMapping(value = "/chat/with/memory", produces = "text/plain", consumes = "application/json")
+    String chatWithMemory(@RequestBody String message) {
+        log.info("Chatting Memory with message: {}", message);
+
+        return chatClient.prompt()
+                .user(message)
+                .advisors(new MessageChatMemoryAdvisor(chatMemory))
                 .call()
                 .content();
     }
--- src/main/java/workshop/springai/config/ChatClientConfig.java
+++ src/main/java/workshop/springai/config/ChatClientConfig.java
@@ -2,6 +2,8 @@ package workshop.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +15,10 @@ public class ChatClientConfig {
         return chatClientBuilder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
+    }
+
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
     }
 }
