d99b407b439d39df74f27b7434befd4038d7d20d
bantunes82 <bantunes82@gmail.com>
2025-02-24 22:47:53 +0100

    updating the exercise 2

---
+++ src/main/java/workshop/springai/chat/ChatMemoryController.java
@@ -0,0 +1,30 @@
+package workshop.springai.chat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ChatMemoryController {
+
+    private final ChatClient chatClient;
+
+    public ChatMemoryController(@Qualifier("chatClientWithMemory") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping(value = "/chat/memory", produces = "text/plain", consumes = "text/plain")
+    String chat(@RequestBody String message) {
+        log.info("Chat Memory with message: {}", message);
+
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+
+}
--- src/main/java/workshop/springai/config/ChatClientConfig.java
+++ src/main/java/workshop/springai/config/ChatClientConfig.java
@@ -1,17 +1,34 @@
 package workshop.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChatClientConfig {
 
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+
     @Bean(name = "chatClient")
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder
                 .defaultAdvisors(
+                        new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    @Bean(name = "chatClientWithMemory")
+    public ChatClient chatClientWithMemory(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        return chatClientBuilder
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory),
                         new SimpleLoggerAdvisor())
                 .build();
     }
