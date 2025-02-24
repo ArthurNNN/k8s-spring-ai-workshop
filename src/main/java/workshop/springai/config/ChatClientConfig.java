af28394debb44829e081d307f0d42ed50e1300e0
bantunes82 <bantunes82@gmail.com>
2025-02-24 22:11:22 +0100

    Updating the Exercise 1

--- src/main/java/workshop/springai/config/ChatClientConfig.java
+++ src/main/java/workshop/springai/config/ChatClientConfig.java
@@ -1,55 +1,19 @@
 package workshop.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.transformer.splitter.TextSplitter;
-import org.springframework.ai.transformer.splitter.TokenTextSplitter;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChatClientConfig {
 
-    @Bean
-    public ChatMemory chatMemory() {
-        return new InMemoryChatMemory();
-    }
-
-    @Bean
-    public TextSplitter textSplitter() {
-        return new TokenTextSplitter();
-    }
-
     @Bean(name = "chatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor())
                 .build();
     }
-
-    @Bean(name = "chatClientWithMemory")
-    public ChatClient chatClientWithMemory(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
-        return chatClientBuilder
-                .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(chatMemory),
-                        new SimpleLoggerAdvisor())
-                .build();
-    }
-
-    @Bean(name = "chatClientWithRag")
-    public ChatClient chatClientWithRag(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
-        return chatClientBuilder
-                .defaultAdvisors(
-                        new QuestionAnswerAdvisor(vectorStore),
-                        new SimpleLoggerAdvisor())
-                .build();
-    }
-
 
 }
--- src/test/java/workshop/springai/TalentArenaApplicationTests.java
+++ src/test/java/workshop/springai/TalentArenaApplicationTests.java
@@ -2,10 +2,7 @@ package workshop.springai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import workshop.springai.config.TestContainersConfiguration;
 
-@Import(TestContainersConfiguration.class)
 @SpringBootTest
 class TalentArenaApplicationTests {
 
